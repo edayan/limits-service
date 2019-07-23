1be1e2c835442584522df807453662519fd7ff2a
Saju Paul <saju.christel@blu-pa.com>
2019-07-23 19:40:12 +0530

    enhance properties to app.properties

--- src/main/java/com/in28minutes/microservice/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservice/limitsservice/LimitsConfigurationController.java
@@ -1,14 +1,18 @@
 package com.in28minutes.microservice.limitsservice;
 
 import com.in28minutes.microservice.limitsservice.bean.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitConfigurations() {
-        return new LimitConfiguration(1000, 1);
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
     }
 }
